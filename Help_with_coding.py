4538e6847a1edc98edbec563e94a33324553c680
Zach Horowitz <zachahorowitz@gmail.com>
2022-11-26 18:47:08 -0500

    testing

---
+++ Help_with_coding.py
@@ -0,0 +1,33 @@
+import os
+os.system ("say Hello")
+# os means operating system
+#___________________________________________________________________________
+# * means times
+# ** is to the power of/exponents
+# + is plus
+# / is dividing
+# += is just like 5 + 10 = 15 or 5 += 10 same thing just makes the e
+# quation smaller
+# syntaxerror means you did something incorrect like you cannot use 
+# upercases it stuff like print.
+# you must use " at the beggining and start in the (" ")
+# like this if not used and you said hello = 10 then wrote down 
+# print (hello) it would say 10 but if you use " like this: print 
+# ("hello") it would only say hello not 10
+# if you did Hi = 10 and then do print (Hi) it says 10 because hello 
+# equals 10
+# you can combine stuff for ex: A = 1 B = 2 then you write print (A +B)
+# it will say 3 becuase 1 plus 2 equals 3
+# upercause and lower case like a = 1 but A = 2 lets say then if I did 
+# upercase 
+# or lowercase it would be what I said upercause or lowercause equals
+# a = 1 A = 2   print (a + A) would be 3 becuase upercause and lower case =
+# something different
+# when saying print you can not have a lower cause but if you say print 
+# ("") you can use upercause in that part of code and that part only
+# if I did 27 % 4 if would say 3  because 27 divided by 4 is 6 with a 
+# remainder of 3 wich is why it is 3
+# if you did A = 1 something and later in the code A = 3
+# would be whatever you said A equaled closest to in the code
+# python always uses pemdas so you may need to use perentices to get the 
+# correct answer or you may get the wrong answer 
---
+++ Idk.py
@@ -0,0 +1,11 @@
+Books = 10
+Coins = 16
+coffee = 5
+pencils = 25
+
+
+x = Books + Coins
+q = coffee + pencils
+x = 26
+q = 30
+print (x + q)
