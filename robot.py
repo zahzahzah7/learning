4ff8deaeff454e13a93993a033e41e2ac0ed7f4a
Eliot Horowitz <eliothorowitz@gmail.com>
2022-11-26 20:20:38 -0500

    robot

---
+++ robot.py
@@ -0,0 +1,26 @@
+import asyncio
+import os
+
+from viam.robot.client import RobotClient
+from viam.rpc.dial import Credentials, DialOptions
+
+async def connect():
+    creds = Credentials(
+        type='robot-location-secret',
+        payload=os.environ["V_PAYLOAD"])
+    opts = RobotClient.Options(
+        refresh_interval=0,
+        dial_options=DialOptions(credentials=creds)
+    )
+    return await RobotClient.at_address(os.environ["V_ROBOT"], opts)
+
+async def main():
+    robot = await connect()
+
+    print('Resources:')
+    print(robot.resource_names)
+
+    await robot.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
